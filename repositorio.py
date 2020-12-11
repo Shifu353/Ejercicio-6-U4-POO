14048705135a0d25ff5563bf86760de65800ae5c
Shifu353 <68873489+Shifu353@users.noreply.github.com>
2020-12-11 13:36:07 -0300

    Create repositorio.py

---
+++ repositorio.py
@@ -0,0 +1,25 @@
+from Provincias import Provincia
+from ManejadorProvincias import ManejadorProvincias
+from ObjectEncoder import ObjectEncoder
+
+class RespositorioProvincias:
+    __prov = None
+    __manejador = None
+
+    def __init__(self, prov):
+        self.__prov = prov
+        diccionario = self.__prov.leerJSONArchivo()
+        self.__manejador = self.__prov.decodificarDiccionario(diccionario)
+    
+    def to_values(self, provincia):
+        return provincia.getNombre(), provincia.getCapital(), provincia.getHabitantes(), provincia.getDepartamentos()
+    
+    def obtenerListaProvincias(self):
+        return self.__manejador.getLista()
+    
+    def agregarProvincia(self, provincia):
+        self.__manejador.agregarProvincia(provincia)
+        return provincia
+
+    def grabarDatos(self):
+        self.__prov.guardarJSONArchivo(self.__manejador.toJSON())
